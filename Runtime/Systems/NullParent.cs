2f26c99d92aeaeaa854b4e86c74952ee5e3f8d51
Matthew Sanders <dev.mattsanders@gmail.com>
2023-07-25 21:58:33 +0100

    Null parent class. A quick way to make sure SetParent(null) doesn't end up filling up the hierarchy. Need to call a special command and add this to the scene to work

---
+++ Runtime/Systems/NullParent.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rufas.BasicFunctions
+{
+    public class NullParent : MonoBehaviour
+    {
+        public static Transform nullParent;
+
+        private void Awake()
+        {
+            if (nullParent == null) nullParent = transform;
+        }
+
+        private void OnDestroy()
+        {
+            if (nullParent == transform) nullParent = null;
+        }
+    }
+}
