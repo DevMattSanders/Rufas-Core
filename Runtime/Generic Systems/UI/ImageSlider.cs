a89da15c656ace306a6a282d153898addb343096
joe-sandfordtech <68084259+scruffduff@users.noreply.github.com>
2023-08-06 12:51:32 +0100

    Updated imaged slider

--- Runtime/Generic Systems/UI/ImageSlider.cs
+++ Runtime/Generic Systems/UI/ImageSlider.cs
@@ -13,17 +13,8 @@ namespace Rufas
         [SerializeField] private Image imageSlider;
         [SerializeField] private Gradient colourGradient;
 
-
-        //private void OnValidate()
-        //{
-        //    if (imageSlider != null) { UpdateSlider(value); }
-        //}
-
-
         public void UpdateSlider(float _value)
         {
-            Debug.Log("Update slider");
-
             value = _value;
             imageSlider.fillAmount = _value;
             imageSlider.color = colourGradient.Evaluate(value);
