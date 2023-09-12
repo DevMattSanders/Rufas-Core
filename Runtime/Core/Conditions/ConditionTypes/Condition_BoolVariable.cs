2b9c2ad31bc057efbe42d68ab9fcc4b548885062
Matthew Sanders <dev.mattsanders@gmail.com>
2023-09-12 14:53:48 +0100

    Update Condition_BoolVariable.cs

--- Runtime/Core/Conditions/ConditionTypes/Condition_BoolVariable.cs
+++ Runtime/Core/Conditions/ConditionTypes/Condition_BoolVariable.cs
@@ -2,7 +2,6 @@ using Sirenix.OdinInspector;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 using UnityEngine;
 using static UnityEngine.CullingGroup;
 
