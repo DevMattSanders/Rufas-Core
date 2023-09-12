91a85a0d04295bb1f6bc65ccdd302a6877feb1e3
joe-sandfordtech <68084259+scruffduff@users.noreply.github.com>
2023-09-12 15:37:30 +0100

    Update QuestTaskCompleter.cs

--- Samples/Quests/QuestTaskCompleter.cs
+++ Samples/Quests/QuestTaskCompleter.cs
@@ -6,11 +6,12 @@ namespace Rufas.Quests
 {
     public class QuestTaskCompleter : MonoBehaviour
     {
+        [SerializeField] private QuestTracker _tracker;
         [SerializeField] private QuestTask task;
 
         public void CompleteTask()
         {
-            if (task != null)
+            if (task != null && _tracker.currentQuest.currentTask == task)
             {
                 task.completeEvent.Raise();
             }
