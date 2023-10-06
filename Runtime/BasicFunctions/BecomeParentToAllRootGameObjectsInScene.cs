ddc89422b3432420d784ddfc36f4d08ef62d78e8
Matthew Sanders <dev.mattsanders@gmail.com>
2023-10-06 18:21:04 +0100

    Update BecomeParentToAllRootGameObjectsInScene.cs

--- Runtime/BasicFunctions/BecomeParentToAllRootGameObjectsInScene.cs
+++ Runtime/BasicFunctions/BecomeParentToAllRootGameObjectsInScene.cs
@@ -7,14 +7,24 @@ namespace Rufas.BasicFunctions
 {
     public class BecomeParentToAllRootGameObjectsInScene : MonoBehaviour
     {
+        [Tooltip("If null, will use this object")]
+        public Transform parentToSet;
+
+        private void Awake()
+        {
+            if (parentToSet == null) parentToSet = transform;
+        }
+
         private void Start()
         {
             transform.SetParent(null);
             transform.SetPositionAndRotation(Vector3.zero,Quaternion.identity);
 
-            foreach(GameObject g in gameObject.scene.GetRootGameObjects())
+           // parentToSet.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
+
+            foreach (GameObject g in gameObject.scene.GetRootGameObjects())
             {
-                g.transform.SetParent(transform, true);
+                g.transform.SetParent(parentToSet, true);
             }
         }
     }
