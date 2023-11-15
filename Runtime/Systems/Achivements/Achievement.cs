a58b4cde509a06ff6e73d0161a4ced26aabce294
joe-sandfordtech <68084259+scruffduff@users.noreply.github.com>
2023-11-15 14:16:55 +0000

    Updated Achievements SO

--- Runtime/Systems/Achivements/Achievement.cs
+++ Runtime/Systems/Achivements/Achievement.cs
@@ -1,3 +1,4 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,5 +10,43 @@ namespace Rufas.Achivements
     {
         public string apiName;
         public bool unlocked = false;
+        [Space]
+        public UnlockType unlockType;
+
+        [ShowIf("unlockType", UnlockType.Count), SerializeField, ReadOnly] private int counter;
+        [ShowIf("unlockType", UnlockType.Count), SerializeField, ReadOnly] private int counterUnlockValue;
+        
+        [ShowIf("unlockType", UnlockType.Bitfield), SerializeField, ReadOnly] private string bitField = "0000000";
+        [ShowIf("unlockType", UnlockType.Bitfield), SerializeField, ReadOnly] private int bitFieldUnlockValue;
+
+        public int GetCounterProgress() {
+            return counter;
+        }
+
+        public string GetBitFieldProgress() { 
+            return bitField;
+        }
+
+        public bool IsUnlocked()
+        {
+            return unlocked;
+        }
+
+        [ShowIf("unlockType", UnlockType.Count), Button()] private void SetCounter(int newValue)
+        {
+            counter = newValue;
+        }
+
+        [ShowIf("unlockType", UnlockType.Bitfield), Button()] private void SetBitfield(string newValue)
+        {
+            bitField = newValue;
+        }
+    }
+
+    public enum UnlockType
+    { 
+        Simple,
+        Count,
+        Bitfield
     }
 }
--- Runtime/Systems/Achivements/OculusIntergration/OculusAchivementGetter.cs
+++ Runtime/Systems/Achivements/OculusIntergration/OculusAchivementGetter.cs
@@ -25,8 +25,6 @@ namespace Rufas.Achivements
 
         private void GetAchivementData()
         {
-            ////Oculus.Platform.Models.AchievementDefinitionList defintionList = Oculus.Platform.Achievements.GetDefinitionsByName(namesArray);
-
             Debug.Log("Getting Achivement Data");
             List<string> namesList = new List<string>();
             foreach (Achievement a in achivements)
@@ -34,61 +32,38 @@ namespace Rufas.Achivements
                 namesList.Add(a.apiName);
             }
             string[] namesArray = namesList.ToArray();
-
-            /*             
-            //This will return a list of all Achievments (as definititons), locked or unlocked.
-            Achievements.GetDefinitionsByName(namesArray).OnComplete(
-           (Message<AchievementDefinitionList> msg) =>
-           {
-               foreach (var achievement in msg.Data)
-               {
-                  //This part will run for all achivements on the server.
-               }
-           }
-             );
-            */
-
-
             
             //This will only return achievments that have any or all progress completed. Achievments with zero progress will not be shown
             Achievements.GetProgressByName(namesArray).OnComplete(
             (Message<AchievementProgressList> msg) =>
-            {
-                Dictionary<string,bool> allAchievmentNamesWithProgress = new Dictionary<string,bool>();
-
-                //If theres no achievment found here that matches any name in the namesArray, the local achievement scriptable should be set to 'locked'.
-                foreach (var achievement in msg.Data)
-                {                      
-                    //This part will only run for achievments that have progress or have been unlocked.
-
-                    //Do something here with an achievment that has either been unlocked or had progress put towards it.   
-                    allAchievmentNamesWithProgress.Add(achievement.Name, achievement.IsUnlocked);
-                }
-
-
-                foreach(Achievement a in achivements)
                 {
-                    if (allAchievmentNamesWithProgress.ContainsKey(a.apiName))
+                    Dictionary<string,bool> allAchievmentNamesWithProgress = new Dictionary<string,bool>();
+
+                    //If theres no achievment found here that matches any name in the namesArray, the local achievement scriptable should be set to 'locked'.
+                    foreach (var achievement in msg.Data)
                     {
-                        //Found achievment on server. Syncing progress with our local achievement scriptable
-                        a.unlocked = allAchievmentNamesWithProgress[a.apiName];
+                        //This part will only run for achievments that have progress or have been unlocked.
+
+                        //Do something here with an achievment that has either been unlocked or had progress put towards it.   
+                        allAchievmentNamesWithProgress.Add(achievement.Name, achievement.IsUnlocked);
                     }
-                    else
+
+                    foreach(Achievement a in achivements)
                     {
-                        //Could not find any progress towards this achievment
-                        a.unlocked = false;
+                        if (allAchievmentNamesWithProgress.ContainsKey(a.apiName))
+                        {
+                            //Found achievment on server. Syncing progress with our local achievement scriptable
+                            a.unlocked = allAchievmentNamesWithProgress[a.apiName];
+                        }
+                        else
+                        {
+                            //Could not find any progress towards this achievment
+                            a.unlocked = false;
+                        }
                     }
                 }
-            }
-              );
+            );
         }
-
-        [Button]
-        private void LockAllAchivement()
-        {
-
-        }
-
     }
 
 }
